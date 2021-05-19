7efdf232fd6f755ba1ff3185cfe7db3514133f73
Alexandr <guravskiy79@gmail.com>
2021-05-19 17:18:02 +0300

    use hibernate commit

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,10 +1,16 @@
 package jm.task.core.jdbc.model;
 
+import lombok.*;
+
 import javax.persistence.*;
-import java.util.Objects;
+
 
 @Entity
+@Data
+@EqualsAndHashCode(of = "id")
 @Table(name = "usr")
+@AllArgsConstructor
+@ToString(of = {"id", "name", "age"})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,60 +33,5 @@ public class User {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Byte getAge() {
-        return age;
-    }
-
-    public void setAge(Byte age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
